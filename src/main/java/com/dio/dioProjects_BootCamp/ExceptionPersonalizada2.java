150aec7d13358d9274350240c1ad172785a5234c
ThiagoST32 <thiago.sandre.trevisan@gmail.com>
2025-01-21 10:50:53 -0400

    Refatoração Exercicio 1

--- src/main/java/com/dio/dioProjects_BootCamp/ExceptionPersonalizada2.java
+++ src/main/java/com/dio/dioProjects_BootCamp/ExceptionPersonalizada2.java
@@ -4,12 +4,12 @@ import com.dio.dioProjects_BootCamp.Exceptions.DivisaoNaoExataException;
 
 import javax.swing.*;
 
-public class ExceptionPersonalizada2{
+public class ExceptionPersonalizada2 {
     public static void main(String[] args) {
-        DivisaoNaoExataException exception = new DivisaoNaoExataException();
 
-        final int[] numerador = {16, 31, 64, 191, 259};
-        final int[] denominador = {2, 4, 0, 8, 10};
+        final int[] numerador = {16, 32, 64, 191, 259};
+        final int[] denominador = {1, 2, 7, 3, 4};
+        int result;
 
         for (int i = 0; i < denominador.length; i++) {
             if (numerador[i] % 2 != 0) try {
@@ -17,10 +17,7 @@ public class ExceptionPersonalizada2{
             } catch (DivisaoNaoExataException e) {
                 e.fillInStackTrace();
             }
-            else if (denominador[i] == 0 ) {
-                JOptionPane.showMessageDialog(null, exception.DivisaoNaoExataExceptionZero(denominador[i]).getMessage());
-            }
-            int result = numerador[i] / denominador[i];
+            result = numerador[i] / denominador[i];
             System.out.println(result);
         }
     }
